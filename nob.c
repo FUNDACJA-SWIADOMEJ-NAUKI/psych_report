d9cd8216d509501f7ac457effb4312d1c0564835
Blackstaff <mateusz.czarnecki92@gmail.com>
2026-08-21 19:31:39 +0200

    Compile psych_report_core into a separate object file

--- nob.c
+++ nob.c
@@ -54,6 +54,24 @@ bool build_deps() {
     return build_whisper() && build_llama();
 }
 
+void link_dynlibs(Nob_Cmd *cmd) {
+    nob_cmd_append(cmd, "-lavformat");
+    nob_cmd_append(cmd, "-lavcodec");
+    nob_cmd_append(cmd, "-lavutil");
+    nob_cmd_append(cmd, "-lswresample");
+    nob_cmd_append(cmd, "-Lbuild");
+    nob_cmd_append(cmd, "-Wl,-rpath,$ORIGIN");
+    nob_cmd_append(cmd, "-Wl,-z,origin");
+    nob_cmd_append(cmd, "-lggml");
+    nob_cmd_append(cmd, "-lggml-base");
+    nob_cmd_append(cmd, "-lggml-cpu");
+    nob_cmd_append(cmd, "-lwhisper");
+    nob_cmd_append(cmd, "-lllama");
+    nob_cmd_append(cmd, "-DLLAMA_SHARED");
+    nob_cmd_append(cmd, "-lstdc++");
+//    nob_cmd_append(&cmd, "-lggml-vulkan");
+}
+
 int main(int argc, char **argv) {
     NOB_GO_REBUILD_URSELF(argc, argv);
     if (!nob_mkdir_if_not_exists(BUILD_FOLDER)) return 1;
@@ -61,29 +79,23 @@ int main(int argc, char **argv) {
     Nob_Cmd cmd = {0};
     nob_cc(&cmd);
     nob_cc_flags(&cmd);
+    nob_cmd_append(&cmd, "-c");
     nob_cmd_append(&cmd, "-Ivendor/whisper.cpp/include");
     nob_cmd_append(&cmd, "-Ivendor/whisper.cpp/ggml/include");
     nob_cmd_append(&cmd, "-Ivendor/llama.cpp/include");
     nob_cmd_append(&cmd, "-Ivendor/llama.cpp/ggml/include");
     nob_cmd_append(&cmd, "-I./");
-    nob_cmd_append(&cmd, "-lavformat");
-    nob_cmd_append(&cmd, "-lavcodec");
-    nob_cmd_append(&cmd, "-lavutil");
-    nob_cmd_append(&cmd, "-lswresample");
-    nob_cmd_append(&cmd, "-Lbuild");
-    nob_cmd_append(&cmd, "-Wl,-rpath,$ORIGIN");
-    nob_cmd_append(&cmd, "-Wl,-z,origin");
-    nob_cmd_append(&cmd, "-lggml");
-    nob_cmd_append(&cmd, "-lggml-base");
-    nob_cmd_append(&cmd, "-lggml-cpu");
-//    nob_cmd_append(&cmd, "-lggml-vulkan");
-    nob_cmd_append(&cmd, "-lwhisper");
-    nob_cmd_append(&cmd, "-lllama");
     nob_cmd_append(&cmd, "-DLLAMA_SHARED");
-    nob_cmd_append(&cmd, "-lstdc++");
-    nob_cc_output(&cmd, BUILD_FOLDER "main");
-    nob_cc_inputs(&cmd, SRC_FOLDER "main.c");
+    link_dynlibs(&cmd);
     nob_cc_inputs(&cmd, SRC_FOLDER "psych_report_core.c");
+    nob_cc_output(&cmd, BUILD_FOLDER "psych_report_core.o");
+    if (!nob_cmd_run(&cmd)) return 1;
+    nob_cc(&cmd);
+    nob_cc_flags(&cmd);
+    nob_cc_inputs(&cmd, SRC_FOLDER "main.c");
+    nob_cc_inputs(&cmd, BUILD_FOLDER "psych_report_core.o");
+    nob_cc_output(&cmd, BUILD_FOLDER "main");
+    link_dynlibs(&cmd);
     if (!nob_cmd_run(&cmd)) return 1;
 
     cmd_free(cmd);
